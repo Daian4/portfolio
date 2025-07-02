c8e337cb07ff140049d6a04dff6791078da23f13
Daiana L <daianalimaep@gmail.com>
2025-07-02 09:35:15 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+  navbar.classList.toggle('active');
+  menuIcon.classList.toggle('bx-x');
+};
+
+window.onscroll = () => {
+  navbar.classList.remove('active');
+  menuIcon.classList.remove('bx-x');
+};
+
+// Interatividade simples: mostra qual stack foi clicada
+document.querySelectorAll('.stack-container img').forEach(icon => {
+  icon.addEventListener('click', () => {
+    alert(`Você clicou em ${icon.alt}!`);
+  });
+});
